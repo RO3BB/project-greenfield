df5d64a3f80d2dd4a9147bed4292d3c31c916dba
karimzargouni <zargouni.karim.kz@gmail.com>
2021-05-12 02:47:11 +0100

    push

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                hello 
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ controllers/userController.js
@@ -0,0 +1,37 @@
+// const { Router } = require('express');
+const express = require('express');
+const User = require('../models/usermodels.js');
+const bcrypt = require('bcrypt');
+var router = express.Router()
+// const { getMaxListeners } = require('../models/usermodels.js');
+
+// router.post('/register', function (req, res) {
+//   console.log("POST Request Received");
+//   res.send('<h2 style="font-family: Malgun Gothic; color: green; ">A course new Course is Added!</h2>');
+// })
+// signup controller
+// const control ={
+// render:  async (req, res) => {
+router.post('/register', async  (req, res) => {
+    try {
+    //     // // const { name, email, password } = req.body;
+    //     // const user = await User.findOne({ email });
+    //     // if (user) return res.status(400).json({ msg: 'email already exists' });
+    //     // if (password.length < 8) return res.status(400).json({ msg: 'short password' });
+
+        //create new user and save it in database
+        const newUser = new User({
+            name: 'nadia',
+            email: 'nadia@gmail.com', 
+            password: '123merde'
+        })
+        await newUser.save()
+        return res.status(200).json({newUser})
+    }
+    catch(err) {
+        return res.status(500).json({ msg: err.message })
+    }
+})
+
+
+module.exports = router
